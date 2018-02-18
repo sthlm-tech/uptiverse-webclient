80121ac7fe3a205bcccd4b121bb33e3152acdeb0
ulfdahlstrand <ulf.d.dahlstrand@gmail.com>
2018-02-18 16:13:54 +0100

    fixe that menus close on link click

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ import React from 'react';
 import Link from '../Link';
 import FontAwesome from 'react-fontawesome';
 import {features} from './features';
-import { toggleMenu } from '../../actions/menu';
+import { toggleMenu, closeMenus } from '../../actions/menu';
 
 class Navigation extends React.Component {
   render() {
@@ -31,14 +31,14 @@ class Navigation extends React.Component {
               if(!item.shouldShow){ return null; }
               return (
                 <div className="menuContentLimiter" key={index}>
-                  <MenuItem item={item}/>
-                  {item.submenu && <SubMenu items={item.submenu}/> }
+                  <MenuItem item={item} onClick={(e)=>{ this.handleMenuItemClick(e)}}/>
+                  {item.submenu && <SubMenu items={item.submenu} onClick={(e)=>{ this.handleMenuItemClick(e)}}/> }
                 </div>
               )
             })
           }
 
-          <a className="menuBlockLink" href="http://authentication.uptiverse.se/authentication/logout?url=http://beta.uptiverse.se">
+          <a className="menuBlockLink" href="http://authentication.uptiverse.se/authentication/logout?url=https://www.uptiverse.se">
             <div className="menuBlock">
               <FontAwesome className="menuIcon" name='sign-out'/>
               <span className="menuText">Logout</span>
@@ -52,12 +52,16 @@ class Navigation extends React.Component {
   handleClick(){
     this.props.dispatch(toggleMenu(this.props.menuName));
   }
+
+  handleMenuItemClick(){
+    this.props.dispatch(closeMenus());
+  }
 }
 
-const MenuItem = ({item}) => {
+const MenuItem = ({item, onClick}) => {
   let iconClass = item.implemented ? "menuIcon": "menuIcon comming";
   return(
-    <Link className="menuBlockLink" to={item.link} >
+    <Link className="menuBlockLink" to={item.link} onClick={onClick}>
       <div className="menuBlock">
         <FontAwesome className={iconClass} name={item.icon}/>
         <span className="menuText">{item.name}</span>
@@ -66,7 +70,7 @@ const MenuItem = ({item}) => {
   );
 }
 
-const SubMenu = ({items}) => {
+const SubMenu = ({items, onClick}) => {
   return (
     <div className="subMenu">
       <FontAwesome className="subMenuIndicator" name="chevron-down"/>
@@ -76,7 +80,7 @@ const SubMenu = ({items}) => {
           if(!item.shouldShow){ return null; }
           return (
             <li key={index}>
-              <MenuItem item={item}/>
+              <MenuItem item={item} onClick={onClick}/>
             </li>
           )
         })
